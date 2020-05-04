ecd38541facab3f2182389d951a550f57665d536
SamMalcolm <sam_malcolm@live.com.au>
2020-05-04 15:19:24 +1000

    finished solution, should move logic to controlelr

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -8,8 +8,18 @@ class DemoController < ApplicationController
 
 	def hello
 		@array = [1,2,3,4,5]
+		@id = params['id']
+		@page = params[:page]
 		# render('index')
 		# redirect_to(:controller => 'demo', :action => 'index')
+	end
+
+	def about
+		render('about_us')
+	end
+
+	def contact
+		render('contact_us')
 	end
 	  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
 	root 'demo#index'
 
   get 'demo/index'
-  get 'demo/hello'
-  #get ':controller(/:action(/:id))'
+  get 'demo/about'
+  get 'demo/contact'
+#   get 'demo/hello'
+  get ':controller(/:action(/:id))'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
