ea32092f728185e7a55024f76b6b227e13775ad6
SamMalcolm <sam_malcolm@live.com.au>
2020-05-19 11:49:33 +1000

    chapter 7 part way through

---
+++ app/controllers/subjects_controller.rb
@@ -0,0 +1,48 @@
+class SubjectsController < ApplicationController
+  def index
+	@subjects = Subject.order('position ASC')
+  end
+
+  def show
+	@subject = Subject.find(params[:id])
+  end
+
+  def new
+	@subject = Subject.new(:visible => false)
+  end
+
+  def create
+	
+	@subject = Subject.new(subject_params)
+	if @subject.save
+		redirect_to(subjects_path)
+	else
+		render('new')
+	end
+  end
+
+  def edit
+	@subject = Subject.find(params[:id])
+  end
+
+  def update
+	@subject = Subject.find(params[:id])
+	if @subject.update(subject_params)
+		redirect_to(subject_path(@subject))
+	else
+		render('edit')
+	end
+
+  end	
+
+  def delete
+  end
+
+  def destroy
+  end
+
+  private
+	  def subject_params
+		params.require(:subject).permit(:name, :visible, :position)
+	  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,38 @@
 Rails.application.routes.draw do
 
-	root 'demo#index'
+
+
+
+  root 'demo#index'
 
   get 'demo/index'
   get 'demo/about'
   get 'demo/contact'
+  get 'demo/hello'
+
+  resources :subjects do
+	member do
+		get :delete
+	end
+  end
+
+  resources :pages do
+	member do
+		get :delete
+	end
+  end
 #   get 'demo/hello'
-  get ':controller(/:action(/:id))'
+#   get ':controller(/:action(/:id))'
+
+  #   get 'pages/index'
+#   get 'pages/show'
+#   get 'pages/new'
+#   get 'pages/edit'
+#   get 'pages/delete'
+#   get 'subjects/index'
+#   get 'subjects/show'
+#   get 'subjects/new'
+#   get 'subjects/edit'
+#   get 'subjects/delete'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
